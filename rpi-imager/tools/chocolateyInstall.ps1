0a8826aaac8bed4ed7a08a50e3829d2a915583ed
Eugene Baranov <220614+eugbaranov@users.noreply.github.com>
2020-05-05 10:47:17 +0100

    Add Raspberry Pi Imager

---
+++ rpi-imager/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url      = 'https://github.com/raspberrypi/rpi-imager/releases/download/v1.2/imager.exe'
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'EXE'
+    url            = $url
+    softwareName   = 'Raspberry Pi Imager'
+    checksum       = 'f2252626d43e1b6340e6d05354d73dbee4f9e859179a4c4c5e584a965ad04c14'
+    checksumType   = 'sha256'
+    silentArgs     = '/S'
+    validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
