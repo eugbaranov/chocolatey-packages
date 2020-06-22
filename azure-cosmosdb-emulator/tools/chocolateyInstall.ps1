5253b982d8bc6a01a27131b5df134cf6126e4859
Eugene Baranov <220614+eugbaranov@users.noreply.github.com>
2020-06-22 12:18:45 +0100

    Azure Cosmos Emulator

---
+++ azure-cosmosdb-emulator/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'msi'
+    url            = ''
+    url64bit       = 'https://cosmosdbportalstorage.azureedge.net/emulator/2020_06_10_2.11.1-01f62a95/azure-cosmosdb-emulator-2.11.1-01f62a95.msi'
+    softwareName   = 'Azure Cosmos DB Emulator'
+    checksum64     = '2fa092e4ad2c7f9adbd8ba57b45a05859bb8102827b1f1671580bbaea927bfd1'
+    checksumType64 = 'sha256'
+    silentArgs     = '/qn /norestart'
+    validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ azure-cosmosdb-emulator/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"       = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+    $Latest.ChecksumType64 = 'sha256'
+}
+
+function global:au_GetLatest {
+    $response = Invoke-WebRequest https://aka.ms/cosmosdb-emulator -MaximumRedirection 0 -ErrorAction SilentlyContinue
+    $url = $response.Headers.Location
+    $parts = $url.Split('-')
+    $version = $parts[$parts.Count - 2]
+
+    @{
+        URL64   = $url
+        Version = $version
+    }
+}
+
+Update-Package -ChecksumFor None
