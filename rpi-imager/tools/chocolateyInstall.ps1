39800fc72d2c6b9d3631da46b461faa0ec3983b6
Chocolatey <chocolatey@realdimensions.net>
2023-03-01 16:06:42 +0000

    AU: 1 updated - rpi-imager [skip ci] https://gist.github.com/dc1e9ae0defb8daba647360ca869eb97/9865e26dcdc6b17ba0ddd537ed725ff7777895c1

--- rpi-imager/tools/chocolateyInstall.ps1
+++ rpi-imager/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://github.com/raspberrypi/rpi-imager/releases/download/v1.7.3/imager_1.7.3.exe'
+$url      = 'https://github.com/raspberrypi/rpi-imager/releases/download/v1.7.4/imager-1.7.4.exe'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
     fileType       = 'EXE'
     url            = $url
     softwareName   = 'Raspberry Pi Imager'
-    checksum       = '88db6e02dbca0bf49e859b89656c9d35c48653c31ca4492dadf7836149f2d854'
+    checksum       = '44e6f70412f0430a372c04d3bd60162d175600ab4da38da8efe20d9be3f37fac'
     checksumType   = 'sha256'
     silentArgs     = '/S'
     validExitCodes = @(0)
