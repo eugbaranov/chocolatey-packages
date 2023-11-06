92b903a89ea85115488308ba48f28d9c8713f5bd
Chocolatey <chocolatey@realdimensions.net>
2023-11-06 00:06:56 +0000

    AU: 1 updated - jellyfin [skip ci] https://gist.github.com/eugbaranov/dc1e9ae0defb8daba647360ca869eb97/dca545abbbf2b2613734c8a96aa6d9d3b7711fe0

--- jellyfin/tools/chocolateyInstall.ps1
+++ jellyfin/tools/chocolateyInstall.ps1
@@ -15,8 +15,8 @@ function CreateShortcut {
 Install-ChocolateyPackage `
     -PackageName $env:ChocolateyPackageName `
     -FileType 'EXE' `
-    -Url64bit 'https://repo.jellyfin.org/releases/server/windows/stable/installer/jellyfin_10.8.11_windows-x64.exe' `
-    -Checksum64 '468304e27c9f865586f82192324bb8982aed09e88464fecd10bb58f6671046bb' `
+    -Url64bit 'https://repo.jellyfin.org/releases/server/windows/stable/installer/jellyfin_10.8.12_windows-x64.exe' `
+    -Checksum64 'e892895b5a0e866f86a5efc4ba1ff1a490db8242ab550abbb3e970414e354598' `
     -ChecksumType64 'sha256' `
     -SilentArgs '/S' `
     -ValidExitCodes @(0,1) `
