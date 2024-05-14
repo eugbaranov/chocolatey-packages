7d6fd315f6813346995dd07e72fd4c15dc146a79
Eugene Baranov <220614+eugbaranov@users.noreply.github.com>
2024-05-14 23:16:15 +0100

    Update Jellyfin auto-update script

--- jellyfin/update.ps1
+++ jellyfin/update.ps1
@@ -11,10 +11,12 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $downloadPage = "https://repo.jellyfin.org/releases/server/windows/stable/"
-    $release = (Invoke-WebRequest -Uri $downloadPage).Links | Where-Object { $_.href -like '*_windows-x64.exe' } | Select-Object -ExpandProperty href
+    $downloadPage = "https://repo.jellyfin.org/files/server/windows/stable/"
+    $latest = (Invoke-WebRequest -Uri $downloadPage).Links | Select-Object -Last 1
+    $releasePage = "$downloadPage$($latest.href)amd64"
+    $release = (Invoke-WebRequest -Uri $releasePage).Links | Where-Object { $_.href -like '*_windows-x64.exe' } | Select-Object -ExpandProperty href
     $version = $release.Split('_')[1]
-    $url = "$downloadPage$release"
+    $url = "$releasePage/$release"
 
     @{
         URL64   = $url
