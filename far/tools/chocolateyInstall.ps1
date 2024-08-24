f7db781595378aa890a2180311638dea5c3a9dfd
Chocolatey <chocolatey@realdimensions.net>
2024-08-24 16:07:21 +0000

    AU: 1 updated - far [skip ci] https://gist.github.com/eugbaranov/dc1e9ae0defb8daba647360ca869eb97/fa4cd67c60ec98b28bcb43d261f016e94d930f3f

--- far/tools/chocolateyInstall.ps1
+++ far/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'msi'
-    url            = 'https://www.farmanager.com/files/Far30b6300.x86.20240407.msi'
-    url64bit       = 'https://www.farmanager.com/files/Far30b6300.x64.20240407.msi'
+    url            = 'https://www.farmanager.com/files/Far30b6364.x86.20240824.msi'
+    url64bit       = 'https://www.farmanager.com/files/Far30b6364.x64.20240824.msi'
     softwareName   = 'Far Manager*'
-    checksum       = 'ed28ab359bef9adce89a06660d04dcba981d8479fcde03db95214e9a6085e66f'
+    checksum       = '3c83ee49e6944baa027cd59d90d52e39b03c709cf1fe6b77bef6261d4003a2a9'
     checksumType   = 'sha256'
-    checksum64     = 'bb54ecf43a539dac9b0c4eb0b95b93b54547036e5d25ac6059eaa2cea26c1f71'
+    checksum64     = '608af6ed1d6ff09d25077c3a7eeca0333241c7cc4e198f83db352fd554a55c1a'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
     validExitCodes = @(0, 3010, 1641)
