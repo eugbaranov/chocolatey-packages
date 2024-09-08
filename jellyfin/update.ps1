9c31dcdef58472c0894929d373914bb419ad160a
Eugene Baranov <220614+eugbaranov@users.noreply.github.com>
2024-09-08 13:45:33 +0100

    Correctly detect Jellyfin version

--- jellyfin/update.ps1
+++ jellyfin/update.ps1
@@ -12,7 +12,10 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $downloadPage = "https://repo.jellyfin.org/files/server/windows/stable/"
-    $latest = (Invoke-WebRequest -Uri $downloadPage).Links | Select-Object -Last 1
+    $releases = (Invoke-WebRequest -Uri $downloadPage).Links
+    $latest = $releases `
+        | Sort-Object -Property { [System.Version]::Parse($_.href.TrimStart('v').TrimEnd('/')) } -Descending -ErrorAction SilentlyContinue `
+        | Select-Object -First 1
     $releasePage = "$downloadPage$($latest.href)amd64"
     $release = (Invoke-WebRequest -Uri $releasePage).Links | Where-Object { $_.href -like '*_windows-x64.exe' } | Select-Object -ExpandProperty href
     $version = $release.Split('_')[1]
