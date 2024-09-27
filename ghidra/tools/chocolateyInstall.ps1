d4a83b2fd082749d0d017acb1f42eea92c61c272
Chocolatey <chocolatey@realdimensions.net>
2024-09-27 16:08:25 +0000

    AU: 1 updated - ghidra [skip ci] https://gist.github.com/eugbaranov/dc1e9ae0defb8daba647360ca869eb97/a9ef3bf956740b7dd08d29f7de9b2f1371f6613e

--- ghidra/tools/chocolateyInstall.ps1
+++ ghidra/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage `
     -PackageName $env:ChocolateyPackageName `
-    -Url64bit 'https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.1.2_build/ghidra_11.1.2_PUBLIC_20240709.zip' `
-    -Checksum64 '219ec130b901645779948feeb7cc86f131dd2da6c36284cf538c3a7f3d44b588' `
+    -Url64bit 'https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.2_build/ghidra_11.2_PUBLIC_20240926.zip' `
+    -Checksum64 'a98fe01038fe8791c54b121ede545ea799d26358794d7c2ac09fa3f5054f3cdc' `
     -ChecksumType64 'sha256' `
     -UnzipLocation $toolsDir
 
