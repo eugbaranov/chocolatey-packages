2520e9f7edc145dc8bccb61de14939455476970c
Julian Dominguez <julido@microsoft.com>
2025-06-02 18:26:56 -0300

    Remove Url parameter with empty value
    The azure-cosmosdb-emulator package is currently awaiting moderation in Chocolatey. While I'm not entirely sure this is the root cause, their documentation say that they validate that if you pass Url, you must also pass Checksum. And if you you pass Url64bit that you also pass Checksum64.
    The point is that you were passing an empty Url without a Checksum.
    So this avoids passing Url entirely, since Url isn't a required argument: https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage/#-url-string

--- azure-cosmosdb-emulator/tools/chocolateyInstall.ps1
+++ azure-cosmosdb-emulator/tools/chocolateyInstall.ps1
@@ -3,7 +3,6 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'msi'
-    url            = ''
     url64bit       = 'https://cdbemulator-dmhwaeevbhd3e9f8.b02.azurefd.net/msi/pipeline/azure-cosmosdb-emulator-2.14.23-19d5c986.msi'
     softwareName   = 'Azure Cosmos DB Emulator'
     checksum64     = 'dd5e8f976f5a6693f3bbaf9cd6d64d99a0008abd34c1416a978a402c80b266b7'
