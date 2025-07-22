d86ab280371f96ac8d066545ab80384dc235aa2e
Chocolatey <chocolatey@realdimensions.net>
2025-07-22 16:07:48 +0000

    AU: 1 updated - azure-cosmosdb-emulator [skip ci] https://gist.github.com/eugbaranov/dc1e9ae0defb8daba647360ca869eb97/39fd5690c975b84016a315881651d7cbe0caa4b7

--- azure-cosmosdb-emulator/tools/chocolateyInstall.ps1
+++ azure-cosmosdb-emulator/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'msi'
-    url64bit       = 'https://cdbemulator-dmhwaeevbhd3e9f8.b02.azurefd.net/msi/pipeline/azure-cosmosdb-emulator-2.14.23-fc82dff5.msi'
+    url64bit       = 'https://cdbemulator-dmhwaeevbhd3e9f8.b02.azurefd.net/msi/pipeline/azure-cosmosdb-emulator-2.14.24-7610c980.msi'
     softwareName   = 'Azure Cosmos DB Emulator'
-    checksum64     = 'bfba892c3b9e27093a6aa36cd3c447d812ca32d6a4361db64e753a63c90c2766'
+    checksum64     = '2c74b0de2328017f35a205ec2d8d14f52613507da9a72a0fc5f64a78e9d887c4'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
     validExitCodes = @(0, 3010, 1641)
