784122ed7ed8ca589c8eb068d9043679d5d569ac
Chocolatey <chocolatey@realdimensions.net>
2025-08-01 00:08:16 +0000

    AU: 2 updated - azure-cosmosdb-emulator ghidra [skip ci] https://gist.github.com/eugbaranov/dc1e9ae0defb8daba647360ca869eb97/b9ca3f0617303f8f8f872eb716cb28eb7cbb90af

--- ghidra/tools/chocolateyInstall.ps1
+++ ghidra/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage `
     -PackageName $env:ChocolateyPackageName `
-    -Url64bit 'https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.4_build/ghidra_11.4_PUBLIC_20250620.zip' `
-    -Checksum64 '8b2902bb3166901bc6297a06892eee077c08605be65abfe73ce7aaaafb651632' `
+    -Url64bit 'https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.4.1_build/ghidra_11.4.1_PUBLIC_20250731.zip' `
+    -Checksum64 '59b657c39c2113d65e591c1087b77d85f6f7a11e97f2f4a0c9e7188510d07ea0' `
     -ChecksumType64 'sha256' `
     -UnzipLocation $toolsDir
 
