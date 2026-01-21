4e7746e07a222faf37647c744f3a09f53e32140f
Chocolatey <chocolatey@realdimensions.net>
2026-01-21 00:07:57 +0000

    AU: 2 updated - azure-cosmosdb-emulator rpi-imager [skip ci] https://gist.github.com/eugbaranov/dc1e9ae0defb8daba647360ca869eb97/320fe39063f56fbbc69d0031fc1b9146822e742b

--- rpi-imager/tools/chocolateyInstall.ps1
+++ rpi-imager/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://github.com/raspberrypi/rpi-imager/releases/download/v2.0.4/imager-v2.0.4.exe'
+$url      = 'https://github.com/raspberrypi/rpi-imager/releases/download/v2.0.5/imager-v2.0.5.exe'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
     fileType       = 'EXE'
     url            = $url
     softwareName   = 'Raspberry Pi Imager'
-    checksum       = '110f1c8a4387157cb5b68f42c692807b659fc2d32b3dfea2f9d0bca340f51785'
+    checksum       = '1fc1ca5cff1b45daeb2197cb7a6c6d8788a846f8329a8a14bdf29f24e31d6769'
     checksumType   = 'sha256'
     silentArgs     = '/verysilent'
     validExitCodes = @(0)
