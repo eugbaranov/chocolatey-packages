ef23f0b114e3b671566cc3b38a975ced6d9257c0
Chocolatey <chocolatey@realdimensions.net>
2026-04-06 00:07:29 +0000

    AU: 2 updated - azure-cosmosdb-emulator jellyfin [skip ci] https://gist.github.com/eugbaranov/dc1e9ae0defb8daba647360ca869eb97/aacdc88b239749ef4a0b59574e302339167984dd

--- jellyfin/tools/chocolateyInstall.ps1
+++ jellyfin/tools/chocolateyInstall.ps1
@@ -15,8 +15,8 @@ function CreateShortcut {
 Install-ChocolateyPackage `
     -PackageName $env:ChocolateyPackageName `
     -FileType 'EXE' `
-    -Url64bit 'https://repo.jellyfin.org/files/server/windows/stable/v10.11.7/amd64/jellyfin_10.11.7_windows-x64.exe' `
-    -Checksum64 'b9d953c4b7e81b7bdf42a1a7c60c44aeffb4adbe4852b3441ce0274b79957501' `
+    -Url64bit 'https://repo.jellyfin.org/files/server/windows/stable/v10.11.8/amd64/jellyfin_10.11.8_windows-x64.exe' `
+    -Checksum64 '4574fdbb6d7f82ec1b3690fb60b8a762515f0846f196ec7c35d59e705ade52cd' `
     -ChecksumType64 'sha256' `
     -SilentArgs '/S' `
     -ValidExitCodes @(0,1) `
